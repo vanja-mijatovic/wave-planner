12ee62dfee7aab0a08813e8f767eecb560427ac9
Vanja Mijatovic <vmijatovic2000@gmail.com>
2023-04-05 23:40:17 +0200

    Initial project structure

---
+++ waveplanner/src/main/java/com/mijatovic/waveplanner/WavePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.mijatovic.waveplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavePlannerApplication.class, args);
+	}
+
+}
